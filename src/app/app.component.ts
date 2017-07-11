7f9525675b3ec3a8de13bb1da15bfbf6af0a21c5
= <=>
2017-07-11 13:07:51 +0530

    Angular Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular 4.0';
+
+  password:string;
+  username:string;
+  Enterusername:string;
+  Enterpassword:string;
+  ishighlighted:boolean;
+  Title:string;
+
+  ngOnInit() {
+
+    this.password="password";
+    this.username="Username";
+    this.ishighlighted=false;
+
+  }
+
+   onclick() {
+    this.Title=this.username+" "+this.password;
+  }
 }
+
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  @Input() applogin:string;
+ 
+  constructor() {
+    
+
+   }
+
+  ngOnInit() {
+
+  
+
+  }
+
+}
